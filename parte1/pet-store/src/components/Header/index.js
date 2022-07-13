1ecfd6c76be5a1ad8db24a1fc77372d3bb94144e
kauealvs <kauealvesdesena@gmail.com>
2022-07-12 23:24:21 -0300

    Create base project and header

---
+++ parte1/pet-store/src/components/Header/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Header.css";
+import logo from "../../assets/img/logo-header.jpeg";
+import login from "../../assets/img/login.png";
+import miniCart from "../../assets/img/miniCart.png";
+import HamburgerIcon from "../../assets/img/Hamburger_icon.svg";
+
+function index() {
+  return (
+    <header>
+      <div className="container-topHeader">
+        <div>
+          <input className="search-header" placeholder="Pesquisar..." />
+        </div>
+        <div className="div-logo">
+          <img src={logo} />
+        </div>
+        <div className="container-login-minicart">
+          <img src={login} />
+          <img src={miniCart} />
+        </div>
+      </div>
+      <div className="container-list">
+        <ul>
+          <li>Categoria lorem</li>
+          <li>Categoria</li>
+          <li>Categoria lorem ipsum</li>
+          <li>Categoria lorem</li>
+          <li>Categoria</li>
+          <li>Categoria lorem ipsum</li>
+          <li>Categoria ipsum</li>
+          <li>
+            <img src={HamburgerIcon} />
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default index;
